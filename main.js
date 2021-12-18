d891035493dfdc61d092a3fb0300bb04793824db
macizomedia <macizomedia@gmail.com>
2021-12-18 17:34:32 +0100

    Setup Basic Landing

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const root = document.querySelector("#root");
 
 window.addEventListener("load", () => {
   root.innerHTML = `
-        <h1>Hello World!</h1>
-        <p>This is my first web application!</p>
+        <h1>Blas Oronoz</h1>
+        <p>Live Portafolio</p>
     `;
 });
