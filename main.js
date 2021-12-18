38fa480c6b1d14412c99832e512cbe0d5c21291a
macizomedia <macizomedia@gmail.com>
2021-12-18 14:42:26 +0100

    Index main

---
+++ main.js
@@ -0,0 +1,8 @@
+const root = document.querySelector("#root");
+
+window.addEventListener("load", () => {
+  root.innerHTML = `
+        <h1>Hello World!</h1>
+        <p>This is my first web application!</p>
+    `;
+});
