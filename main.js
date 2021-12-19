c04a009536d0dafc481f43fb22bce93537f3a7b3
macizomedia <macizomedia@gmail.com>
2021-12-19 19:06:34 +0100

    update content

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const root = document.querySelector("#root");
 
 window.addEventListener("load", () => {
-  root.innerHTML = `
-        <h1>Blas Oronoz</h1>
-        <p>Live Portafolio</p>
-    `;
-});
+  fetch("./content.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      return (root.innerHTML = `
+              <div class="card">
+                  <h2>${data.name}</h2>
+                  <p>${data.title}</p>
+              </div>
+              <div class="card">
+                  <h2>${JSON.stringify(data.experiance)}</h2>
+              </div>
+          `);
+    });
+}); 
