2ede04bd716ac1ccb7bd5f6c6c6e370e4f7e7a14
Blas Oronoz <42347194+macizomedia@users.noreply.github.com>
2024-10-11 15:55:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const root = document.querySelector("#root");
 
 function main() {
     const h1 = document.createElement("h1");
-    h1.textContent = "Abquanta Initiative";
+    h1.textContent = "The Abquanta Initiative";
     root.appendChild(h1);
 }
 
