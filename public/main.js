9e2a557db9d0c9b075fcec05e24cec5b2e843fcc
Blas Oronoz <info@macizomedia.com>
2024-10-12 13:56:36 +0200

    new test

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 var root = document.getElementById("root");
 window.addEventListener("load", function () {
     console.log("Hello World");
-    root.innerHTML = "devCreative";
+    root.innerHTML = "The Abquanta Initiative";
 });
