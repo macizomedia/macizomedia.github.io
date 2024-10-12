035a22c13986953694ac6912959d16f7c0d7501d
macizomedia <macizomedia@users.noreply.github.com>
2024-10-12 21:59:45 +0000

    deploy: ff902016889cb6af954bc1130f2923eaef6387cf

--- main.js
+++ main.js
@@ -2,5 +2,5 @@
 const root = document.getElementById("root");
 window.addEventListener("load", () => {
     console.log("Getting Abquanted ...");
-    root.innerHTML = `The Abquanta Initiative`;
+    root.innerHTML = `The AbQuanta Initiative is an ambitious idea...`;
 });
