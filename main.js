d1e646bf778795835d7719327bd06c2b678278ab
macizomedia <macizomedia@users.noreply.github.com>
2024-10-12 21:57:22 +0000

    deploy: 17f133ff408eec7eb829746aa7a7995bfdada1cc

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+const root = document.getElementById("root");
+window.addEventListener("load", () => {
+    console.log("Getting Abquanted ...");
+    root.innerHTML = `The Abquanta Initiative`;
+});
