d9098313c3470dd0b912c5d3cc71d48978ccbcd3
macizomedia <macizomedia@gmail.com>
2025-03-25 13:39:43 +0100

    Wiped repository for a fresh start

--- setupTests.js
+++
@@ -1,3 +0,0 @@
-// setupTests.js
-import '@testing-library/jest-dom';
-
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-const root = document.getElementById("root");
-
-window.addEventListener("load", () => {
-  console.log("Getting Abquanted ...");
-  root!.innerHTML = `The AbQuanta Initiative is an ambitious idea...`;
-});
