9d19f159af5fbf511a5adee6d05b59a3bd425bed
macizomedia <macizomedia@gmail.com>
2025-03-25 15:35:42 +0100

    Actions Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  distDir: "out",
   // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
+  },
+  env: {
+    CLIENT_ID: process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID,
   },
 };
 
